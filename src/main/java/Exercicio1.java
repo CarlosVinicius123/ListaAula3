f5f51f54ebb173b324e61f5728b7d0f39efbda64
CarlosVinicius123 <carlosvinnibrandao@gmail.com>
2022-03-04 02:04:18 -0300

    Resolvendo o Exercicio1

---
+++ src/main/java/Exercicio1.java
@@ -0,0 +1,14 @@
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        int numZumbi1 = 120;
+        int numZumbi2 = 240;
+        int numZumbi3 = 320;
+
+        int numZumbis = numZumbi1 + numZumbi2 + numZumbi3;
+
+        System.out.println("Total de zumbis: " + numZumbis);
+        System.out.println("Media de zumbis mortos : " + numZumbis/3);
+
+    }
+}
